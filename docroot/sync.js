b7cd5fb4b787da59b2a9c017a9461534eb90a76c
tigerfarm <tigerfarm@gmail.com>
2019-06-12 17:05:56 -0700

    updates

--- docroot/sync.js
+++ docroot/sync.js
@@ -79,7 +79,7 @@ function getToken() {
             logger('Loading board data.');
             var data = syncDoc.value;
             if (data.board) {
-                updateUserInterface(data);
+                updateBoardSquares(data);
             }
             //
             logger('Subscribed to updates for Sync document : ' + syncDocumentName);
@@ -92,14 +92,89 @@ function getToken() {
                 }
                 logger(theMessage + syncEvent.value.useridentity);
                 logger('Sync document JSON data: ' + JSON.stringify(syncEvent.value));
-                updateUserInterface(syncEvent.value);
+                updateBoardSquares(syncEvent.value);
             });
         });
     });
 }
 
+function documentSubscribe() {
+    var syncDocumentName = $("#syncDocumentName").val();
+    if (syncDocumentName === "") {
+        $("#mSyncDocumentName").html("Required");
+        logger("Required: Game name (Sync document name).");
+        return;
+    }
+    // -------------------------------------------------------------------------
+    // The game board data is stored in a Sync document.
+    // Subscribe to the document; or if it doesn't exist, create it.
+    // 
+    syncClientObject.document(syncDocumentName).then(function (syncDoc) {
+        thisSyncDoc = syncDoc;
+        logger('Loading board data.');
+        var data = syncDoc.value;
+        if (data.board) {
+            updateBoardSquares(data);
+        }
+        //
+        logger('Subscribed to updates for Sync document : ' + syncDocumentName);
+        syncDoc.on('updated', function (syncEvent) {
+            theMessage = '';
+            if (syncEvent.isLocal) {
+                theMessage = "Updated by this player: ";
+            } else {
+                theMessage = "Updated by another player: ";
+            }
+            logger(theMessage + syncEvent.value.useridentity);
+            logger('Sync document JSON data: ' + JSON.stringify(syncEvent.value));
+            updateBoardSquares(syncEvent.value);
+        });
+    });
+}
+
+function updateSyncDocument() {
+    if (thisIdentity === "") {
+        $("#mUserIdentity").html("Required");
+        logger("Required: user identity.");
+        return;
+    }
+    var currentBoard = readGameBoardFromUserInterface();
+    logger('currentBoard JSON data: ' + JSON.stringify(currentBoard));
+    thisSyncDoc.set(currentBoard);
+}
+
+function getGame() {
+    if (thisIdentity === "") {
+        $("#mUserIdentity").html("Required");
+        logger("Required: user identity.");
+        return;
+    }
+    var syncDocumentName = $("#syncDocumentName").val();
+    if (syncDocumentName === "") {
+        $("#mSyncDocumentName").html("Required");
+        logger("Required: Game name.");
+        return;
+    }
+    aClearBoard = {"board":[["","",""],["","",""],["","",""]],"useridentity":thisIdentity};
+    logger('aClearBoard JSON data: ' + JSON.stringify(aClearBoard));
+    updateBoardSquares(aClearBoard);
+    documentSubscribe()();
+}
+
 // -------------------------------------------------------------------------
 // HTML Tic-Tac Board Functions
+
+function clearBoard() {
+    if (thisIdentity === "") {
+        $("#mUserIdentity").html("Required");
+        logger("Required: user identity.");
+        return;
+    }
+    aClearBoard = {"board":[["","",""],["","",""],["","",""]],"useridentity":thisIdentity};
+    logger('aClearBoard JSON data: ' + JSON.stringify(aClearBoard));
+    updateBoardSquares(aClearBoard);
+    updateSyncDocument();
+}
 
 function buttonClick() {
     $square = $(event.target);
@@ -111,11 +186,7 @@ function buttonClick() {
     } else {
         $square.html('X');
     }
-    var data = readGameBoardFromUserInterface();
-    //
-    // Update the Sync document.
-    // logger('Button clicked, thisSyncDoc: ' + thisSyncDoc.uniqueName + ' : ' + JSON.stringify(data));
-    thisSyncDoc.set(data);
+    updateSyncDocument();
 }
 
 //Read the state of the UI and create a new document
@@ -137,9 +208,9 @@ function readGameBoardFromUserInterface() {
     return {board: board, useridentity: thisIdentity};
 }
 
-//Update the squares on the board to match our document
-function updateUserInterface(data) {
-    // logger('updateUserInterface()');
+// Update the squares on the board to match the data.
+function updateBoardSquares(data) {
+    // logger('updateBoardSquares()');
     for (var row = 0; row < 3; row++) {
         for (var col = 0; col < 3; col++) {
             var this_cell = '[data-row="' + row + '"]' + '[data-col="' + col + '"]';
